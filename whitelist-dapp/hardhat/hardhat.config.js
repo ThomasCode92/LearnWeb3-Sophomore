4d3952b91211536ef252870eace1c28b0d02bedf
ThomasCode92 <thomascode92@hotmail.com>
2023-08-02 12:54:55 +0200

    Environment Variables & Deployment

--- whitelist-dapp/hardhat/hardhat.config.js
+++ whitelist-dapp/hardhat/hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+const dotenv = require("dotenv");
+
+// Load Environment variables
+const { parsed } = dotenv.config();
+const { PRIVATE_KEY, QUICKNODE_HTTP_URL, ETHERSCAN_API_KEY } = parsed;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
