82255eec65b141b00729a3b1911e2945dfa19c13
ThomasCode92 <thomascode92@hotmail.com>
2023-12-11 19:23:43 +0100

    Creating the DAO - Part 3 (Basic Type Declarations & State Variables)

--- onchain-dao/hardhat/contracts/CryptoDevsDAO.sol
+++ onchain-dao/hardhat/contracts/CryptoDevsDAO.sol
@@ -8,5 +8,33 @@ import './interfaces/IFakeNFTMarketplace.sol';
 import './interfaces/ICryptoDevsNFT.sol';
 
 contract CryptoDevsDAO is Ownable {
-    // We will write contract code here
+    // A struct containing all relevant information about a proposal
+    struct Proposal {
+        // nftTokenId - the tokenID of the NFT to purchase from
+        // FakeNFTMarketplace if the proposal passes
+        uint256 nftTokenId;
+        // deadline - the UNIX timestamp until which this proposal is active.
+        // Proposal can be executed after the deadline has been exceeded.
+        uint256 deadline;
+        // yayVotes - number of yay votes for this proposal
+        uint256 yayVotes;
+        // nayVotes - number of nay votes for this proposal
+        uint256 nayVotes;
+        // executed - whether or not this proposal has been executed yet.
+        // Cannot be executed before the deadline has been exceeded.
+        bool executed;
+        // voters - a mapping of CryptoDevsNFT tokenIDs to booleans indicating
+        // whether that NFT has already been used to cast a vote or not
+        mapping(uint256 => bool) voters;
+    }
+
+    // A mapping of ID to proposal
+    mapping(uint256 => Proposal) public proposals;
+
+    // Number of proposals that have been created
+    uint256 public numProposals;
+
+    // Reference to the FakeNFTMarketplace and CryptoDevsNFT contracts
+    IFakeNFTMarketplace nftMarketplace;
+    ICryptoDevsNFT cryptoDevsNFT;
 }
