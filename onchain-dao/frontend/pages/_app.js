01612b442695bab65b63baa5f19e17000c71b9d3
ThomasCode92 <thomascode92@hotmail.com>
2024-01-04 08:30:53 +0100

    Setting up Wallet Connection

--- onchain-dao/frontend/pages/_app.js
+++ onchain-dao/frontend/pages/_app.js
@@ -1,5 +1,31 @@
-import '@/styles/globals.css'
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { sepolia } from 'wagmi/chains';
+
+import '@rainbow-me/rainbowkit/styles.css';
+import '@/styles/globals.css';
+
+const { chains, publicClient } = configureChains([sepolia], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: 'CryptoDevs DAO',
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
