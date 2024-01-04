7eb476d12464e40585d8991070186c1f983f70de
ThomasCode92 <thomascode92@hotmail.com>
2024-01-04 08:30:43 +0100

    Define all the Constants

---
+++ onchain-dao/frontend/utils/constants.js
@@ -0,0 +1,14 @@
+import cryptoDevsNFT from '../../hardhat/artifacts/contracts/CryptoDevsNFT.sol/CryptoDevsNFT.json';
+import fakeNFTMarketplace from '../../hardhat/artifacts/contracts/FakeNFTMarketplace.sol/FakeNFTMarketplace.json';
+import cryptoDevsDAO from '../../hardhat/artifacts/contracts/CryptoDevsDAO.sol/CryptoDevsDAO.json';
+
+export const CryptoDevsNFTAddress =
+  '0x547DDd8dBdc5D1fB4A50918F73476BAF01ef74a2';
+export const FakeNFTMarketplaceAddress =
+  '0xD88DB09A87681d1E9Cb9D27810A892abb43308A9';
+export const CryptoDevsDAOAddress =
+  '0x1C2027e14c41c4079d0aE9DE0039c0c5C9310600';
+
+export const CryptoDevsNFTABI = cryptoDevsNFT.abi;
+export const FakeNFTMarketplaceABI = fakeNFTMarketplace.abi;
+export const CryptoDevsDAOABI = cryptoDevsDAO.abi;
