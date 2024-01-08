ab05025ea6c050164f3c73cbdf362d5869c32602
ThomasCode92 <thomascode92@hotmail.com>
2024-01-08 07:31:54 +0100

    Add & Remove Liquidity Functions

--- dex-app/hardhat/contracts/Exchange.sol
+++ dex-app/hardhat/contracts/Exchange.sol
@@ -18,4 +18,84 @@ contract Exchange is ERC20 {
     function getReserve() public view returns (uint256) {
         return ERC20(tokenAddress).balanceOf(address(this));
     }
+
+    /// @dev Allow users to add liquidity to the exchange
+    /// @param amountOfToken The amount of `token` to add to the exchange
+    /// @return uint256 representing the amount of LP tokens minted
+    function addLiquidity(
+        uint256 amountOfToken
+    ) public payable returns (uint256) {
+        uint256 lpTokensToMint;
+        uint256 ethReserveBalance = address(this).balance;
+        uint256 tokenReserveBalance = getReserve();
+
+        ERC20 token = ERC20(tokenAddress);
+
+        // If the reserve is empty, take any user supplied value for initial liquidity
+        if (tokenReserveBalance == 0) {
+            // Transfer the token from the user to the exchange
+            token.transferFrom(msg.sender, address(this), amountOfToken);
+
+            // lpTokensToMint = ethReserveBalance = msg.value
+            lpTokensToMint = ethReserveBalance;
+
+            // Mint LP tokens to the user
+            _mint(msg.sender, lpTokensToMint);
+
+            return lpTokensToMint;
+        }
+
+        // If the reserve is not empty, calculate the amount of LP Tokens to be minted
+        uint256 ethReservePriorToFunctionCall = ethReserveBalance - msg.value;
+        uint256 minTokenAmountRequired = (msg.value * tokenReserveBalance) /
+            ethReservePriorToFunctionCall;
+
+        require(
+            amountOfToken >= minTokenAmountRequired,
+            'Insufficient amount of tokens provided'
+        );
+
+        // Transfer the token from the user to the exchange
+        token.transferFrom(msg.sender, address(this), minTokenAmountRequired);
+
+        // Calculate the amount of LP tokens to be minted
+        lpTokensToMint =
+            (totalSupply() * msg.value) /
+            ethReservePriorToFunctionCall;
+
+        // Mint LP tokens to the user
+        _mint(msg.sender, lpTokensToMint);
+
+        return lpTokensToMint;
+    }
+
+    /// @dev Allow users to remove liquidity from the exchange
+    /// @param amountOfLPTokens The amount of LP tokens to remove from the user
+    /// @return uint256 representing the amount of ETH returned to the user
+    /// @return uint256 representing the amount of tokens returned to the user
+    function removeLiquidity(
+        uint256 amountOfLPTokens
+    ) public returns (uint256, uint256) {
+        // Check that the user wants to remove >0 LP tokens
+        require(
+            amountOfLPTokens > 0,
+            'Amount of tokens to remove must be greater than 0'
+        );
+
+        uint256 ethReserveBalance = address(this).balance;
+        uint256 lpTokenTotalSupply = totalSupply();
+
+        // Calculate the amount of ETH and tokens to return to the user
+        uint256 ethToReturn = (ethReserveBalance * amountOfLPTokens) /
+            lpTokenTotalSupply;
+        uint256 tokenToReturn = (getReserve() * amountOfLPTokens) /
+            lpTokenTotalSupply;
+
+        // Burn the LP tokens from the user, and transfer the ETH and tokens to the user
+        _burn(msg.sender, amountOfLPTokens);
+        payable(msg.sender).transfer(ethToReturn);
+        ERC20(tokenAddress).transfer(msg.sender, tokenToReturn);
+
+        return (ethToReturn, tokenToReturn);
+    }
 }
